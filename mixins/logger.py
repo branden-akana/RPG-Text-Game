19ca1f637f03b7e79273c43fbbc74b8cc5c4bcb4
Branden Akana <branden.akana@gmail.com>
2020-06-19 19:00:34 -1000

    implement turn system, redesign input logic for player, add create_action function

---
+++ mixins/logger.py
@@ -0,0 +1,39 @@
+
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class Message():
+    text: str
+    style: str = 'normal'
+    fg: int = 0
+    bg: int = 0
+
+
+class Logger:
+    """An object that can log messages."""
+
+    def __init__(self, log_limit: int = 5):
+
+        # the max amount of messages to display
+        self.log_limit = log_limit
+
+        # contains all the logged messages
+        self.log_messages: List[Message] = []
+
+    def get_log(self) -> List[Message]:
+        return self.log_messages
+
+    def log(self, text: str, fg=15, bg=0, style='normal'):
+        """Log a message."""
+
+        # insert to the front of the list
+        self.log_messages.insert(0, Message(text, style, fg, bg))
+
+        # delete last message if above log_limit
+        if len(self.log_messages) > self.log_limit:
+            del self.log_messages[self.log_limit]
+
+    def info(self, text: str):
+        self.log(text, 8)  # dark gray
---
+++ turns.py
@@ -0,0 +1,58 @@
+
+from entity import LivingEntity
+from typing import (
+    List,
+    TYPE_CHECKING
+)
+
+
+if TYPE_CHECKING:
+    from game import Game
+
+
+class TurnManager:
+    """A manager for the turn order of entities."""
+
+    def __init__(self, game: 'Game', ents: 'List[LivingEntity]'):
+
+        self.game = game
+        self.ents = ents
+        self.order = []
+        self.idx = 0
+        self.last_idx = 0
+
+    def start_order(self):
+        """Decide the turn order of the given list of entities."""
+
+        # for now just use the same order as the list
+        order = [ent for ent in self.ents
+                if isinstance(ent, LivingEntity)]
+
+        # if len(order):
+            # self.game.info(f'** order: {[ent.name for ent in order]} **')
+
+        self.idx = 0
+        self.last_idx = 0
+        self.order = order
+
+    def get_last_entity(self) -> LivingEntity:
+        return self.order[self.last_idx]
+
+    def get_current_entity(self) -> LivingEntity:
+        return self.order[self.idx]
+
+    def run_next(self):
+        """Run the next turn in the order."""
+
+        if len(self.order) > 0:
+
+            ent = self.order[self.idx]
+
+            if ent.is_alive():
+                # self.game.info(f'it is now {ent.name} turn')
+                action = ent.think()
+                if action: action.do_action()
+
+            # increment index
+            self.last_idx = self.idx
+            self.idx = (self.idx + 1) % len(self.order)
