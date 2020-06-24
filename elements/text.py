b3989004e14ee0f7de92279115d326f38b6c9636
Branden Akana <branden.akana@gmail.com>
2020-06-23 15:58:57 -1000

    WIP: add default colorscheme if pywal doesn't exist

--- elements/text.py
+++ elements/text.py
@@ -10,26 +10,37 @@ if TYPE_CHECKING:
     from game import Game
 
 
-def get_wal_colors():
-    """Attempt to get the current colors used by pywal."""
+def get_colors():
 
-    res = subprocess.run(['cat', os.path.expanduser('~/.cache/wal/colors')],
-                         stdout=subprocess.PIPE)
-    lines = res.stdout
-    colors = []
+    try:  # attempt to get the current colors used by pywal.
+        res = subprocess.run(['cat', os.path.expanduser('~/.cache/wal/colors')],
+                             stdout=subprocess.PIPE)
+        lines = res.stdout
+        colors = []
 
-    if lines:
-        for line in lines.splitlines():
-            # print('{} {} {}'.format(line[1:3], line[3:5], line[5:7]))
-            r = int(line[1:3], 16)
-            g = int(line[3:5], 16)
-            b = int(line[5:7], 16)
-            colors.append((r, g, b))
+        if lines:
+            for line in lines.splitlines():
+                # print('{} {} {}'.format(line[1:3], line[3:5], line[5:7]))
+                r = int(line[1:3], 16)
+                g = int(line[3:5], 16)
+                b = int(line[5:7], 16)
+                colors.append((r, g, b))
 
-    return colors
+        return colors
+
+    except Exception:
+        return [(0, 0, 0),
+                (255, 255, 255),
+                (255, 255, 255),
+                (255, 255, 255),
+                (255, 255, 255),
+                (255, 255, 255),
+                (255, 255, 255),
+                (255, 255, 255)
+                ] * 2
 
 
-_colors: list = get_wal_colors()
+_colors: list = get_colors()
 
 
 class Text:
@@ -60,7 +71,8 @@ class Text:
 
         self.e_background = pyglet.shapes.Rectangle(
             x - padding/2, y - padding/2,
-            self.e_label.content_width + padding, self.e_label.content_height + padding,
+            self.e_label.content_width + padding,
+            self.e_label.content_height + padding,
             color=self.bg_color
         )
 
@@ -70,5 +82,5 @@ class Text:
 
 
 if __name__ == '__main__':
-    # test get_wal_colors()
-    get_wal_colors()
+    # test get_colors()
+    get_colors()
